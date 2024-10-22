cf575d3eff4ae1fb9bf7ed5060a418fc2537762c
Ellie <151094173+EllieM-gd@users.noreply.github.com>
2024-10-22 13:46:20 -0700

    Step 2

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,39 @@ const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
 
 const canvas = document.createElement("canvas");
-//const ctx = canvas.getContext("2d")!; // Rendering Context, Will be used later
+const ctx = canvas.getContext("2d")!; // Rendering Context
+const cursor = { active: false, x: 0, y: 0 }; //Mouse Cursor
 
-app.appendChild(canvas);
+app.append(canvas)
+
+canvas.addEventListener("mousedown", (event) => {
+    cursor.active = true;
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+});
+
+canvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+    if (cursor.active) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        cursor.x = event.offsetX;
+        cursor.y = event.offsetY;
+    }
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
